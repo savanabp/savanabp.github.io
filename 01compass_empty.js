140830fbafa649465316a8424637f9792b7ead17
Savana Burdick-Perez <savanaburdick-perez@Savanas-MacBook-Pro.local>
2018-02-13 14:34:05 -0600

    Initial commit

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -9,19 +9,19 @@ oriented = function(e) {
         }
 
 
-        if (compassHeading > 315 || compassHeading < 45) {
+        if(compassHeading > 315 || compassHeading < 45) {
             text_area.textContent = "north";
         }
         
-        if (compassHeading >= 45 && compassHeading < 135) {
+        if(compassHeading >= 45 && compassHeading < 135) {
             text_area.textContent = "east";
         }
 
-        if (compassHeading >= 135 && compassHeading < 225) {
+        if(compassHeading >= 135 && compassHeading < 225) {
             text_area.textContent = "south";
         }
 
-        if (compassHeading >= 225 && compassHeading < 315) {
+        if(compassHeading >= 225 && compassHeading < 315) {
             text_area.textContent = "west";
         }
 
