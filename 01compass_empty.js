c8ce603d8780769ed52ef86362e3a6741fbf40c3
Savana Burdick-Perez <savanaburdick-perez@Savanas-MacBook-Pro.local>
2018-02-13 14:15:52 -0600

    Initial commit

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -7,8 +7,13 @@ oriented = function(e) {
         }   else  { 
             compassHeading = e.alpha;
         }
+
+
+        if (compassHeading > 315 || compassHeading < 45) {
+            text_area.textContent = compassHeading;
+        }
         
-         text_area.textContent = compassHeading;
+        // text_area.textContent = compassHeading;
         //calls function to alter content based on heading
        // myOrientation();
     }
